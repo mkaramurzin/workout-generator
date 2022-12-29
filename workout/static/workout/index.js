03d8fb79936a92559b81e6f38d9cf14fdf1a9cd9
unknown <mkaramurzin02@gmail.com>
2022-12-29 14:14:24 -0800

    Prompt engineering and API request

---
+++ workout/static/workout/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#generate').onclick = function() {
+
+        document.querySelector('#generate').style.display = 'none';
+        document.querySelector('#loading').style.display = 'block';
+
+    }
+
+})
--- workout/views.py
+++ workout/views.py
@@ -1,4 +1,38 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import requests
+import openai
 
+@csrf_exempt
 def index(request):
-    return render(request, 'workout/index.html')
+    if request.method == 'GET':
+        return render(request, 'workout/index.html')
+
+    else:
+        prompt = "Imagine you are a personal fitness trainer. You are knowledgable and descriptive.\n\n"
+        prompt += f"Client: I weigh {request.POST['weight']}" 
+        if request.POST['location']:
+            prompt += f"pounds and I will be exercising {request.POST['location']} {request.POST['frequency']} times a week."
+        prompt += f"\nClient: My goal is {request.POST['goal']}\n"
+        if request.POST['extra'] != '':
+            prompt += f"Client: Some important additional information is that {request.POST['extra']}\n"
+        
+        prompt += f"Client: Create a detailed workout plan for me."
+
+        print(prompt)
+
+        openai.api_key = "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"
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+        )
+
+        print(response.choices[0].text)
+        return render(request, 'workout/result.html', {
+            'plan': response.choices[0].text
+        })
